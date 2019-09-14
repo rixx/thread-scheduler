d4a689ff0249995fbc5080d680109a0bf480c639
Tobias Kunze <r@rixx.de>
2019-09-14 01:27:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Thread Scheduler
+
+No creative names for you, I'm afraid. This is a script to post pre-configured threads on Twitter.
+
+## Setup
+
+To set it up, get yourself a virtualenv of any kind, and ``pip install -r requirements.txt``.
+
+You'll need to [register an app](https://developers.twitter.com). This page has okay
+[instructions](https://python-twitter.readthedocs.io/en/latest/getting_started.html).
+
+Put a file called ``settings.py`` in this directory and put in your ``API_KEY``, ``API_SECRET``, ``AUTH_TOKEN_KEY`` and
+``AUTH_TOKEN_SECRET``.
+
+## Tweets
+
+You'll need one JSON file per thread. A file looks like this:
+
+```json
+{
+  "start": "2019-09-14T20:00:00",
+  "tweets": [
+    {
+      "text": "Something"
+    },
+    {
+      "text": "Something",
+      "offset": 60  // Interval between the previous tweet and this one in seconds
+    },
+    {
+      "text": "Tweet\nwith\nnewlines"
+      "offset": 3
+    }
+  ]
+}
+```
+
+The file will later be edited by Thread Scheduler – **please keep a backup copy**.
+
+## Usage
+
+Check what would happen if you were to run Thread Scheduler first:
+
+```
+python thread_scheduler.py tweets.json --check
+```
+
+Maybe post a single tweet first (useful if you want to run this via crontab):
+
+```
+python thread_scheduler.py tweets.json --one-off
+```
+
+Or just leave the script running until the thread has been posted. Will provide print output, and save progress in the
+JSON file, so that you can safely abort at any time:
+
+```
+python thread_scheduler.py tweets.json
+```
---
+++ thread_scheduler.py
@@ -0,0 +1,144 @@
+"""
+This tool helps you schedule Twitter threads.
+
+Each thread is its own JSON file. Run this tool like this:
+
+    python thread-scheduler.py path/to/file [--check] [--one-off]
+
+The JSON file will be updated by this script. It looks like this:
+
+    {
+        "tweets": [
+            {
+                "text": "…",
+            },
+            {
+                "text": "…",
+                "offset": 60  // Seconds in between this and the previous tweet
+            },
+        ],
+        "start": "2019-08-13T20:00:00"
+    }
+
+Put a settings.py next to this file with your API_KEY, API_SECRET,
+ACCESS_TOKEN_KEY and ACCESS_TOKEN_SECRET.
+"""
+import datetime as dt
+import json
+import sys
+import time
+
+import dateutil.parser
+import tweepy
+
+from settings import API_KEY, API_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET
+
+
+def json_datetime(value):
+    if isinstance(value, dt.datetime):
+        return value.isoformat()
+    raise ValueError
+
+
+class Thread:
+    def __init__(self, path):
+        self.path = path
+        self.load()
+        self.check()
+
+    def load(self):
+        with open(self.path) as fp:
+            config = json.load(fp)
+        self.start = dateutil.parser.parse(config["start"])
+        self.tweets = [
+            {
+                "text": tweet["text"],
+                "sent": dateutil.parser.parse(tweet["sent"])
+                if tweet.get("sent")
+                else None,
+                "offset": tweet.get("offset"),
+                "twitter_id": tweet.get("twitter_id"),
+            }
+            for tweet in config["tweets"]
+        ]
+
+    def check(self):
+        last_tweet = None
+        for tweet in self.tweets:
+            text = tweet["text"]
+            if len(text) > 280:
+                raise Exception("Tweet cannot be longer than 280 characters: " + text)
+            if text == last_tweet:
+                raise Exception(
+                    "Twitter will probably prevent two same tweets in a row: " + text
+                )
+            last_tweet = text
+
+    def save(self):
+        with open(self.path, "w") as fp:
+            json.dump(
+                {"start": self.start, "tweets": self.tweets},
+                fp,
+                default=json_datetime,
+                indent=4,
+            )
+
+    def get_to_be_sent(self):
+        last_sent = None
+        last_id = None
+        for tweet in self.tweets:
+            if tweet["sent"]:
+                last_sent = tweet["sent"]
+                last_id = tweet["twitter_id"]
+                continue
+            return (
+                tweet,
+                last_sent + dt.timedelta(seconds=tweet["offset"])
+                if last_sent
+                else None,
+                last_id,
+            )
+
+
+def main():
+    path = sys.argv[1]
+    thread = Thread(path)
+    check = "--check" in sys.argv
+    one_off = "--one-off" in sys.argv
+    if not check:
+        auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+        auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+        api = tweepy.API(auth)
+
+    while thread.get_to_be_sent():
+        tweet, timestamp, twitter_id = thread.get_to_be_sent()
+        now = dt.datetime.now()
+        if timestamp and timestamp > now:
+            sleep_duration = (timestamp - now).total_seconds()
+            sleep_minutes = int(sleep_duration / 60)
+            sleep_seconds = sleep_duration % 60
+            if check:
+                print(
+                    f"Would sleep for {sleep_minutes} minutes, {sleep_seconds} seconds, then post:"
+                )
+                print(tweet["text"])
+                return
+            print(f"Will sleep for {sleep_minutes} minutes, {sleep_seconds} seconds.")
+            time.sleep(sleep_duration)
+        if check:
+            print("Would post this tweet:")
+            print(tweet["text"])
+            return
+        result = api.update_status(tweet["text"], in_reply_to_status_id=twitter_id)
+        tweet["twitter_id"] = result.id
+        tweet["sent"] = now
+        thread.save()
+        print("Sent out a tweet:")
+        print(tweet["text"])
+        if one_off:
+            return
+    print("All tweets have been sent!")
+
+
+if __name__ == "__main__":
+    main()
